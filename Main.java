6523a84ef2b4f964efafc10df4e7b8e13667fb44
swathinemaan <115583283+swathinemaan@users.noreply.github.com>
2022-11-14 11:34:52 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,23 +1,18 @@
-package com.greatlearning.bst.driver;
+package com.greatlearning.skyscraper.driver;
 
-import com.greatlearning.model.Node;
-import com.greatlearning.model.TreeConversion;
+import com.greatlearning.skrscraper.model.ConstructionOrder;
+import com.greatlearning.skrscraper.model.MyConsole;
 
 public class Main {
-	
-
-		public static void main(String[] args) {
-			
-			Node inputInformation=new Node(50);
-			inputInformation.left = new Node(30);
-			inputInformation.right = new Node(60);
-			inputInformation.left.left = new Node(10);
-			inputInformation.right.left= new Node(55);
-
-			TreeConversion treeConversion=new TreeConversion();
-			treeConversion.bstToSkewedConversion(inputInformation, 0);
-			treeConversion.printSkewedTree();
+public static void main(String[] args) {
+		
+		int totalFloors=MyConsole.getIntegerValues("Enter the Total No of floors:");
+		int[] floorsConstruction=new int[totalFloors]; 
+		for (int i = 0; i < totalFloors; i++) {
+			floorsConstruction[i]=MyConsole.getIntegerValues("enter the floor size given on day "+ (i+1) + ":");	
 		}
-	
-
+		ConstructionOrder constructionOrder=new ConstructionOrder();
+		constructionOrder.Construction(floorsConstruction);
+		
+	}
 }
